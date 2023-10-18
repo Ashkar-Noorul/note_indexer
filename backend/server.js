e54838ea7ac226c300779c392fa3bb833dd816a3
Ashkar-Noorul <noorulashkar007@gmail.com>
2023-10-18 18:49:49 +0530

    deployment scripts

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,31 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/UserRoutes");
 const noteRoutes = require("./routes/NoteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// --------------------------deployment------------------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
